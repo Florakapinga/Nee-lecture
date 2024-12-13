92c2a06c06bb40e6b98dc286e63d143d93ce1e4f
kapinga <erickfloriana@gmail.com>
2024-12-13 09:55:10 -0800

    second commit

--- main.cpp
+++ main.cpp
@@ -18,3 +18,13 @@ enum eDirection
     DOWN
 };
 eDirection dir;
+void Setup()
+{
+    gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
+}
