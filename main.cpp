10ef950e25c66bf096480e8fb471d81dcdd065b0
kapinga <erickfloriana@gmail.com>
2024-12-13 10:39:57 -0800

    conditions

--- main.cpp
+++ main.cpp
@@ -118,6 +118,7 @@ void Logic()
         x--;
         break;
     case RIGHT:
+
         x++;
         break;
     case UP:
@@ -129,3 +130,24 @@ void Logic()
     default:
         break;
     }
+    if (x >= width)
+        x = 0;
+    else if (x < 0)
+        x = width - 1;
+    if (y >= height)
+        y = 0;
+    else if (y < 0)
+        y = height - 1;
+
+    for (int i = 0; i < nTail; i++)
+        if (tailX[i] == x && tailY[i] == y)
+            gameOver = true;
+
+    if (x == fruitX && y == fruitY)
+    {
+        score += 10;
+        fruitX = rand() % width;
+        fruitY = rand() % height;
+        nTail++;
+    }
+}
